d77181c08bc2310680b8d1da5458a1068d398456
Khôi Nguyễn <minhkhoi260199@gmail.com>
2022-04-25 10:16:57 +0700

    format model k150

--- demo01/src/main/java/com/example/demo01/entities/K150.java
+++ demo01/src/main/java/com/example/demo01/entities/K150.java
@@ -1,5 +1,8 @@
 package com.example.demo01.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +11,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Data
@@ -16,17 +20,23 @@ import java.util.List;
 @Document
 public class K150 {
     @Id
+    @JsonProperty("id")
+    @JsonSerialize(using = ToStringSerializer.class)
     private ObjectId id;
     private String kv151;
     private List<Fk151> fk151 = new ArrayList<>();
     private String pk100;
+    //Record deleted
+    private Date dl145;
 
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
     @Document
     public static class Fk151 {
-        @Id
+        @JsonProperty("id")
+        @JsonSerialize(using = ToStringSerializer.class)
+        ObjectId _id;
         private String kv152;
         private String lang;
     }
--- demo01/src/main/java/com/example/demo01/repositories/K150Repo.java
+++ demo01/src/main/java/com/example/demo01/repositories/K150Repo.java
@@ -1,5 +1,6 @@
 package com.example.demo01.repositories;
 
+import com.example.demo01.entities.K100;
 import com.example.demo01.entities.K150;
 
 import java.util.List;
@@ -7,6 +8,7 @@ import java.util.List;
 public interface K150Repo {
     public K150 find(String id);
     public List<K150> findAll();
-    public K150 insert(K150 k150);
+    K150 queryById(String id);
+    public K150 save(K150 k150);
     public K150 delete(String id);
 }
